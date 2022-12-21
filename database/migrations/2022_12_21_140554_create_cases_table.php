a53c8cc1555e8223f8a05c4b5b927d1634c9ff0c
Amandine Miceli <amandine.miceli@job2day.fr>
2022-12-21 19:24:11 +0100

    Added migration file for Cases table

---
+++ database/migrations/2022_12_21_140554_create_cases_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cases', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->string('reporterName');
+            $table->unsignedTinyInteger('reporterAge');
+            $table->string('reportedUrl');
+            $table->string('reporterEmail')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cases');
+    }
+}
